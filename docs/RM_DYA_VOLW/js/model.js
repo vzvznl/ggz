adf7d1f5baaf98cf7228b05febb29bd59aa4b2ce
MarcelSchrauwen <marcel.schrauwen@vzvz.nl>
2023-03-24 13:15:01 +0100

    update
    Wat kleine foutcorrecties

--- docs/RM_DYA_VOLW/js/model.js
+++ docs/RM_DYA_VOLW/js/model.js
@@ -50,14 +50,14 @@ $(document).ready(function() {
       resizable: false
     },
     west: {
-			size: 350,
+			size: 500,
 			spacing_open: 8
 		},
     west__childOptions: {
       maskContents: true,
       south: {
 	      minSize: 50,
-				size: 250,
+				size: 400,
 				spacing_open: 8
 			},
 			center: {
